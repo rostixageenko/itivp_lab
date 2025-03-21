be6c9ac6911e72d9ca71050d750ac11b17f07c5c
rostixageenko <slavaageenko2005@gmail.com>
2025-03-21 19:56:19 +0300

    finished interactive menu

---
+++ js/open_burger.js
@@ -0,0 +1,8 @@
+const burger = document.getElementById('burger');
+      const menu = document.getElementById('menu');
+
+      burger.addEventListener('click', () => {
+          const isOpen = menu.style.display === 'block';
+          menu.style.display = isOpen ? 'none' : 'block';
+          burger.classList.toggle('open', !isOpen);
+      });
