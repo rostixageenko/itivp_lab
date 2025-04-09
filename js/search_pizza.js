bef87132faf768fa88f240621f6a7de8d1b962c8
rostixageenko <slavaageenko2005@gmail.com>
2025-04-09 17:32:42 +0300

    add filter

--- js/search_pizza.js
+++ js/search_pizza.js
@@ -3,29 +3,48 @@ const searchButton= document.getElementById('search-button');
 const blocks = Array.from(document.querySelectorAll('.block'));
 const clearButton = document.getElementById('clear-button');
 
-searchButton.addEventListener('click', ()=>{
-    const filter = searchInput.value.toLowerCase();
+searchButton.addEventListener('click', () => {
+    const inputValue = searchInput.value; 
+    const trimmedValue = inputValue.trim(); 
+
+    const spacePattern = /^(?! )[^\s]*(\s[^\s]+)*$/; 
+    const maxLength = 30;
+
+    if (inputValue.startsWith(' ') ) {
+        alert('Ошибка: Не начинайте с пробела '); 
+        return;
+    }
+
+    if ( !spacePattern.test(trimmedValue) ) {
+        alert('Ошибка: Вводите не более 1 пробела между словами'); 
+        return;
+    }
+    if ( trimmedValue.length > maxLength) {
+        alert('Ошибка: Не превышайте 30 символов.'); 
+        return;
+    }
+
+    const filter = trimmedValue.toLowerCase();
 
     blocks.forEach(block => {
         const title = block.querySelector('.textic.textmain').textContent.toLowerCase();
-        if(title.includes(filter)){
+        if (title.includes(filter)) {
             block.style.display = '';
         } else {
-            block.style.display= 'none';
+            block.style.display = 'none';
         }
     });
 });
 
 
 clearButton.addEventListener('click', () => {
-    searchInput.value = ''; // Очищаем поле ввода
-    clearButton.style.display = 'none'; // Скрываем крестик
+    searchInput.value = ''; 
+    clearButton.style.display = 'none'; 
     blocks.forEach(block => {
-        block.style.display = ''; // Показываем все блоки
+        block.style.display = ''; 
     });
 });
 
-// Показываем крестик, когда поле ввода не пустое
 searchInput.addEventListener('input', () => {
     if (searchInput.value) {
         clearButton.style.display = 'block';
